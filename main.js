3eedb721cd7ebf9ab7486678c20b3185394ce9ed
Advik7 <75883877+Advik7@users.noreply.github.com>
2021-02-05 09:36:26 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
+const minute_h1 = document.getElementById('minutes').innerText;
+const second_h1 = document.getElementById('seconds').innerText
+
+
+num = 60
+function minute() {
+   minute_h1 = num
+   num--
+
+
+
+}
+
+function second() {
+  second_h1 = num
+  num --
+
+
+}
+
+minute()
+second()
+
+setInterval(second, 1000)
+setInterval( minute, 60000)
 
