279be2999af57df4b530ec9570715ec5e64f0f42
Advik7 <75883877+Advik7@users.noreply.github.com>
2022-01-08 12:32:12 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,67 @@
-function format(num) {
-    if (num < 10) {
-        num = "0" + num   
-    }
-    return num
+"use strict";
+let choices = ["👊", "✋", "✌"];
+let playerChoice = "" ;  
+let computerChoice;
+let rock = document.getElementById("rock");
+let paper = document.getElementById("paper");
+let scissors = document.getElementById("scissors");
+let playerPick = document.getElementById('playerPick')
+let computerPick = document.getElementById('computerPick')
+let resultText = document.getElementById('result')
+let playerScore = 0
+let computerScore = 0
+let state = ""
 
-
+function randomChoice() {
+    let randomNumber = Math.floor(Math.random() * 3)
+    return randomNumber
 
 }
-function time() {
-    var seconds = new Date().getSeconds();
-    var minutes = new Date().getMinutes();
-    var hours = new Date().getHours();
-    var state = "AM"
-    if (hours>12) {
-        hours-=12
-        state = "PM"
 
+rock.addEventListener('click', function() {
+    playerChoice = "👊"
+    gameLogic()
+})
+
+paper.addEventListener('click', function() {
+    playerChoice = "✋"
+    gameLogic()
+})
+
+scissors.addEventListener('click', function() {
+    playerChoice = "✌"
+    gameLogic()
+})
+
+
+
+function gameLogic() {
+    computerChoice = choices[randomChoice()]
+    playerPick.textContent = playerChoice
+    computerPick.textContent = computerChoice
+    if (playerChoice==="👊" && computerChoice ==="✌") {
+        state = "You won"
+        playerScore += 1
+    }else if (playerChoice==="✋" && computerChoice ==="👊") {
+        state = "You won"
+        playerScore += 1
+    }else if (playerChoice==="✌" && computerChoice ==="✋") {
+        state = "You won"
+        playerScore += 1
+    }else if (playerChoice===computerChoice) {
+        state = "It's a draw"
+    }else {
+        console.log("you lost and computer pick " + computerChoice)
+        state = "You lost"
+        computerScore +=1
     }
-    document.querySelector('h1').innerHTML = format(hours) + " : " +  format(minutes) + " : " + format(seconds) + "  " + state
+    resultText.textContent = state + " and the Score is " + playerScore + "-" + computerScore
 }
-time()
-setInterval(time)
+
+
+
+
+
+
+
 
